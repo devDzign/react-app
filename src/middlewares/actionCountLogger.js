e452fb65a1f95db71ec6bcdb4df0676d7945bb57
mourad <chabour.mourad87@gmail.com>
2019-10-29 12:03:52 +0100

    Middlewware: refacto write

--- src/middlewares/actionCountLogger.js
+++ src/middlewares/actionCountLogger.js
@@ -2,13 +2,9 @@ import * as actionType from '../store/actions/actionTypes'
 import {incrementActionCount} from "../store/actions/actions";
 
 
-export const actionCounter = function (store) {
-    return function (next) {
-        return function (action) {
-            if (actionType.INCREMENT_ACTION8COUNT !== action.type) {
-                store.dispatch(incrementActionCount())
-            }
-            next(action)
-        }
+export const actionCounter = (store) => (next) => (action) => {
+    if (actionType.INCREMENT_ACTION8COUNT !== action.type) {
+        store.dispatch(incrementActionCount())
     }
+    next(action)
 }
